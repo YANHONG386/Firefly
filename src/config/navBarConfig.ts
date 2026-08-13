ee2dccda1a1d0e31d344eb3247997a13c54bd8c3
LI YANHONG <113451804+YANHONG386@users.noreply.github.com>
2026-08-13 20:16:46 +0800

    feat: 更新导航栏个人链接(GitHub/Gitee/QQ)

--- src/config/navBarConfig.ts
+++ src/config/navBarConfig.ts
@@ -96,19 +96,19 @@ const getDynamicNavBarConfig = (): NavBarConfig => {
 		children: [
 			{
 				name: "GitHub",
-				url: "https://github.com/CuteLeaf/Firefly",
+				url: "https://github.com/YANHONG386",
 				external: true,
 				icon: "fa7-brands:github",
 			},
 			{
 				name: "Gitee",
-				url: "https://gitee.com/CuteLeaf/Firefly",
+				url: "https://gitee.com/yh486",
 				external: true,
 				icon: "fa7-brands:gitee",
 			},
 			{
-				name: "QQ交流群",
-				url: "https://qm.qq.com/q/ZGsFa8qX2G",
+				name: "QQ",
+				url: "https://qm.qq.com/q/HMeNNe7TEI",
 				external: true,
 				icon: "fa7-brands:qq",
 			},
